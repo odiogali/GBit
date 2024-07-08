c0f6870185989edcaf5a79964666ff75ff2228d6
Odi Ogali <odi.ogali1380@gmail.com>
2024-07-08 16:02:54 -0600

    Temporary structures for how commits and file structure is tracked.

--- main.go
+++ main.go
@@ -4,6 +4,37 @@ import (
 	"fmt"
 )
 
+type readable interface {
+	isDir() bool
+}
+
+type blob struct {
+	name     string
+	path     string
+	contents []byte
+}
+
+type tree struct {
+	name     string
+	path     string
+	children []readable
+}
+
+type commit struct {
+	parents  []commit
+	author   string
+	message  string
+	snapshot tree
+}
+
+func (b blob) isDir() bool {
+	return false
+}
+
+func (t tree) isDir() bool {
+	return true
+}
+
 func main() {
 	fmt.Println("Hello World!")
 }
