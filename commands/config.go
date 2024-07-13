924054d5a6aa85ac68f21898aeac83f99c4d899e
Odi Ogali <odi.ogali1380@gmail.com>
2024-07-12 19:43:29 -0600

    Full config functionality (no --global flags)

--- commands/config.go
+++ commands/config.go
@@ -12,7 +12,7 @@ type User struct {
 }
 
 func Config(args []string) {
-	file, err := os.OpenFile("config.json", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0766)
+	file, err := os.OpenFile("config.json", os.O_CREATE|os.O_RDWR, 0766)
 	if err != nil {
 		panic(err)
 	}
@@ -26,7 +26,8 @@ func Config(args []string) {
 
 	if fileInfo.Size() == 0 {
 		if len(args) == 1 {
-			fmt.Println("Cannot obtain value: ", args[0])
+			fmt.Println("You must specify this value: ", args[0])
+
 		} else if len(args) == 2 && args[0] == "user.email" {
 			u := User{"", args[1]}
 
@@ -53,19 +54,89 @@ func Config(args []string) {
 			}
 		} else {
 			fmt.Println("Too many arguments were provided.")
-			return
 		}
+		return
 	}
+
+	// read config.json to byte slice
+	data, err := os.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var u User
 
 	if len(args) == 2 {
 		if args[0] == "user.name" {
+			// clear the file before writing
+			if err := os.Truncate("config.json", 0); err != nil {
+				fmt.Printf("Failed to truncate: %v", err)
+			}
+
+			err := json.Unmarshal(data, &u)
+			if err != nil {
+				panic(err)
+			}
+
+			u.Name = args[1]
+
+			info, err := json.Marshal(u)
+			if err != nil {
+				panic(err)
+			}
+
+			_, err = file.Write(info)
+			if err != nil {
+				panic(err)
+			}
 		} else if args[0] == "user.email" {
+			// clear the file before writing
+			if err := os.Truncate("config.json", 0); err != nil {
+				fmt.Printf("Failed to truncate: %v", err)
+			}
+
+			err := json.Unmarshal(data, &u)
+			if err != nil {
+				panic(err)
+			}
+
+			u.Email = args[1]
+
+			info, err := json.Marshal(u)
+			if err != nil {
+				panic(err)
+			}
+
+			_, err = file.Write(info)
+			if err != nil {
+				panic(err)
+			}
 		} else {
 			fmt.Println("Subcommand: ", args[0], " not supported.")
 		}
 	} else if len(args) == 1 {
 		if args[0] == "user.name" {
+			err := json.Unmarshal(data, &u)
+			if err != nil {
+				panic(err)
+			}
+
+			if u.Name != "" {
+				fmt.Println(u.Name)
+			} else {
+				fmt.Println("You must specify this value.")
+			}
 		} else if args[0] == "user.email" {
+			err := json.Unmarshal(data, &u)
+			if err != nil {
+				panic(err)
+			}
+
+			if u.Email != "" {
+				fmt.Println(u.Email)
+			} else {
+				fmt.Println("You must specify this value.")
+			}
 		} else {
 			fmt.Println("Subcommand: ", args[0], " not supported.")
 		}
