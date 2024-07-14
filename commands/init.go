f79523b7432ceb31d74267ae9c29ff0773be9c39
Odi Ogali <odi.ogali1380@gmail.com>
2024-07-13 20:15:59 -0600

    First part of init: the folder

--- commands/init.go
+++ commands/init.go
@@ -2,8 +2,24 @@ package commands
 
 import (
 	"fmt"
+	"os"
 )
 
 func Init(args []string) {
-	fmt.Printf("Init got the arguments: %v", args)
+	// Create folder GBit info will be stored in
+	wd, _ := os.Getwd()
+	var folder string
+
+	if len(args) == 0 {
+		folder = wd + "/.GBit"
+	} else if len(args) == 1 {
+		folder = args[0] + "/.GBit"
+	}
+
+	if err := os.Mkdir(folder, 0755); os.IsExist(err) {
+		fmt.Println("Reinitialized existing GBit repository in ", wd)
+	} else if err == nil {
+		fmt.Println("Initialized empty GBit repository in ", folder)
+	}
+
 }
