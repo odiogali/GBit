e714e278bd5dcf02af5240f661e60cd3dd3f3f45
Odi Ogali <odi.ogali1380@gmail.com>
2024-07-22 23:40:12 -0600

    Some compile errors and probably logic too but this is the general idea for huffman.

--- commands/add.go
+++ commands/add.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+var huffCodes map[string]string
+
 func Add(args []string) {
 	// what happens when we call 'add' and not in GBit repository
 	wd, _ := os.Getwd()
@@ -20,17 +22,22 @@ func Add(args []string) {
 
 	if len(args) == 0 {
 		fmt.Println("Nothing specified, nothing added.")
+		os.Exit(1)
 	} else if len(args) == 1 {
 
+		// Adds all files in working directory to necessary GBit repo
 		if args[0] == "." {
+
 			// WARNING: Need to walkthrough all files in working directory
-		} else {
+
+		} else { // Just adds specified file to GBit repo
 
 			dat, err := os.ReadFile(args[0]) // find, read, and store file contents
 			if err != nil {
 				fmt.Printf("fatal: pathspec '%s' did not match any files\n", args[0])
 				os.Exit(1)
 			}
+
 			hashed := hash(dat)                       // hash the file's contents
 			asciiString := hex.EncodeToString(hashed) // name of new file needs to be readable
 			hashedStringPath := objectsDir + "/" + asciiString
@@ -50,15 +57,15 @@ func Add(args []string) {
 				}
 
 				defer file.Close()
-				os.Chdir(wd) // no error handling beccause we know 'wd' exists
+				os.Chdir(wd)
 				// WARNING: Still need to write to the file
 				freq := countFreq(dat)
-				fmt.Println(freq)
+				encodedText, tree := encode(freq, dat)
 			}
 
 		}
 
-	} else {
+	} else { // if number of arguments is not one
 		for _, item := range args {
 			dat, err := os.ReadFile(item) // find, read, and store file contents
 			if err != nil {
@@ -115,6 +122,74 @@ func countFreq(data []byte) map[string]int {
 	return freq
 }
 
+func encode(frequencies map[string]int, unencodedText []byte) (string, huffTree) {
+	huffEntities := []huffEntity{}
+	for key, value := range frequencies {
+		aLeaf := leaf{key, value, nil, nil}
+		huffEntities = append(huffEntities, aLeaf)
+	}
+
+	finalTree := huffTree{}     // there's no real reason for me to be using a huffTree struct here tbh
+	for len(huffEntities) > 1 { // Go doesn't have while loop lol
+		smallest, currentSlice := getSmallestItem(huffEntities)
+		secondSmallest, updatedSlice := getSmallestItem(currentSlice)
+		huffEntities = updatedSlice
+
+		// create node with relevant children and frequency
+		newNode := node{smallest, secondSmallest, (smallest.getFrequency() + secondSmallest.getFrequency())}
+
+		// add node to the huffman tree
+		finalTree.root = newNode
+		huffEntities = append(huffEntities, newNode)
+	}
+
+	generateHuffCodes(finalTree.root, "")
+
+	return getEncodedText(unencodedText), finalTree
+}
+
+// Gets the smallest huffEntity in the slice and returns that and the modified slice
+func getSmallestItem(arr []huffEntity) (huffEntity, []huffEntity) {
+	smallestItem := arr[0]
+	index := 0
+	for i, item := range arr {
+		if item.getFrequency() < smallestItem.getFrequency() {
+			smallestItem = item
+			index = i
+		}
+	}
+
+	if index+1 == len(arr) {
+		arr = append(arr[:index])
+	} else {
+		arr = append(arr[:index], arr[index+1:]...)
+	}
+
+	return smallestItem, arr
+}
+
+func generateHuffCodes(entity huffEntity, huffcode string) {
+	if entity.isLeaf() {
+		huffCodes[entity.(leaf).getChar()] = huffcode
+	}
+
+	if entity.getLeft() != nil {
+		generateHuffCodes(entity.getLeft(), huffcode+"0")
+	}
+
+	if entity.getRight() != nil {
+		generateHuffCodes(entity.getRight(), huffcode+"1")
+	}
+}
+
+func getEncodedText(unencodedText []byte) string {
+	for _, item := range unencodedText {
+		char := string(item)
+		// NOTE: Use bit writer and append bit by bit
+	}
+	return "" // return the result
+}
+
 type node struct {
 	left      huffEntity
 	right     huffEntity
@@ -122,12 +197,21 @@ type node struct {
 }
 
 type leaf struct {
-	character string
-	frequency int
+	character   string
+	frequency   int
+	left, right huffEntity
 }
 
+type huffTree struct {
+	root huffEntity
+}
+
+// for storing both leaves and nodes in the same data structure
 type huffEntity interface {
 	isLeaf() bool
+	getFrequency() int
+	getLeft() huffEntity
+	getRight() huffEntity
 }
 
 func (n node) isLeaf() bool {
@@ -136,4 +220,32 @@ func (n node) isLeaf() bool {
 
 func (l leaf) isLeaf() bool {
 	return true
+}
+
+func (n node) getFrequency() int {
+	return n.frequency
+}
+
+func (l leaf) getFrequency() int {
+	return l.frequency
+}
+
+func (l leaf) getChar() string {
+	return l.character
+}
+
+func (l leaf) getLeft() huffEntity {
+	return nil
+}
+
+func (l leaf) getRight() huffEntity {
+	return nil
+}
+
+func (n node) getLeft() huffEntity {
+	return n.left
+}
+
+func (n node) getRight() huffEntity {
+	return n.right
 }
