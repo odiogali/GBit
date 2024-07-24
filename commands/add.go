cfd6abaaf35dad3522f602d677db8ba384b30a0f
Odi Ogali <odi.ogali1380@gmail.com>
2024-07-24 16:07:56 -0600

    Huffman coding is now fully functional

--- commands/add.go
+++ commands/add.go
@@ -1,9 +1,12 @@
 package commands
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"github.com/icza/bitio"
 	"os"
 )
 
@@ -38,7 +41,7 @@ func Add(args []string) {
 				os.Exit(1)
 			}
 
-			hashed := hash(dat)                       // hash the file's contents
+			hashed := Hash(dat)                       // hash the file's contents
 			asciiString := hex.EncodeToString(hashed) // name of new file needs to be readable
 			hashedStringPath := objectsDir + "/" + asciiString
 
@@ -57,23 +60,50 @@ func Add(args []string) {
 				}
 
 				defer file.Close()
-				os.Chdir(wd)
-				// WARNING: Still need to write to the file
+
+				// Actually write to file
 				freq := countFreq(dat)
-				_, _ = encode(freq, dat)
-				fmt.Println("This is huffCodes after all is said and done: ", huffCodes)
+				encodedText := encode(freq, dat)
+				// fmt.Println("This is huffCodes after all is said and done: ", huffCodes)
+
+				// Write encoded text
+				if _, err := file.Write(encodedText); err != nil {
+					panic(err)
+				}
+
+				var codesStruct = JsonCodes{huffCodes} // create struct for writing to json file
+				jsonData, err := json.Marshal(codesStruct)
+				if err != nil {
+					fmt.Printf("Failed to marshal: %s", args[0])
+				}
+
+				// create new json file and write to it
+				jsonFile, err := os.Create(asciiString + ".json")
+				if err != nil {
+					fmt.Println("Create error (json for decoding): ", error)
+					return
+				}
+				if _, err = jsonFile.Write(jsonData); err != nil {
+					panic(err)
+				}
+
+				// WARNING: testing decode function
+				Decode(asciiString, asciiString+".json")
+
+				os.Chdir(wd)
 			}
 
 		}
 
 	} else { // if number of arguments is not one
 		for _, item := range args {
+			huffCodes = make(map[string]string)
 			dat, err := os.ReadFile(item) // find, read, and store file contents
 			if err != nil {
 				fmt.Printf("fatal: pathspec '%s' did not match any files\n", item)
 				os.Exit(1)
 			}
-			hashed := hash(dat)                       // hash the file's contents
+			hashed := Hash(dat)                       // hash the file's contents
 			asciiString := hex.EncodeToString(hashed) // name of new file needs to be readable
 			hashedStringPath := objectsDir + "/" + asciiString
 
@@ -87,21 +117,45 @@ func Add(args []string) {
 
 				file, error := os.Create(asciiString)
 				if error != nil {
-					fmt.Println("Create error: ", error)
+					fmt.Println("Create file error: ", error)
 					return
 				}
 
 				defer file.Close()
-				os.Chdir(wd) // no error handling beccause we know 'wd' exists
-				// WARNING: Still need to write to the file
+
+				// Actually write to file
 				freq := countFreq(dat)
-				fmt.Println(freq)
+				encodedText := encode(freq, dat)
+
+				// Write encoded text
+				if _, err := file.Write(encodedText); err != nil {
+					panic(err)
+				}
+
+				var codesStruct = JsonCodes{}
+				codesStruct = JsonCodes{huffCodes} // create struct for writing to json file
+				jsonData, err := json.Marshal(codesStruct)
+				if err != nil {
+					fmt.Printf("Failed to marshal: %s", args[0])
+				}
+
+				// create new json file and write to it
+				jsonFile, err := os.Create(asciiString + ".json")
+				if err != nil {
+					fmt.Println("Create error (json for decoding): ", error)
+					return
+				}
+				if _, err = jsonFile.Write(jsonData); err != nil {
+					panic(err)
+				}
+
+				os.Chdir(wd)
 			}
 		}
 	}
 }
 
-func hash(fileContents []byte) []byte {
+func Hash(fileContents []byte) []byte {
 	h := sha256.New()
 	h.Write(fileContents)
 	bs := h.Sum(nil)
@@ -120,17 +174,19 @@ func countFreq(data []byte) map[string]int {
 			freq[letterAsString] = num + 1
 		}
 	}
-	fmt.Println("Should contain characters and their frequencies: ", freq)
+	// Change the following code to an assert perhaps
+	// fmt.Println("Should contain characters and their frequencies: ", freq)
 	return freq
 }
 
-func encode(frequencies map[string]int, unencodedText []byte) (string, huffTree) {
+func encode(frequencies map[string]int, unencodedText []byte) []byte {
 	huffEntities := []huffEntity{}
 	for key, value := range frequencies {
 		aLeaf := leaf{key, value, nil, nil}
 		huffEntities = append(huffEntities, aLeaf)
 	}
-	fmt.Println("Should contain just the leaves: ", huffEntities)
+	// Change the following code to an assert perhaps
+	// fmt.Println("Contains just the leaves: ", len(huffEntities) == len(frequencies))
 
 	finalTree := huffTree{}     // there's no real reason for me to be using a huffTree struct here tbh
 	for len(huffEntities) > 1 { // Go doesn't have while loop lol
@@ -145,12 +201,13 @@ func encode(frequencies map[string]int, unencodedText []byte) (string, huffTree)
 		finalTree.root = newNode
 		huffEntities = append(huffEntities, newNode)
 	}
-	fmt.Println("Slice should have one value: ", huffEntities)
-	fmt.Println("Slice has one value: ", len(huffEntities) == 1)
+	// Change the following code to an assert perhaps
+	// fmt.Println("Slice should have one value: ", huffEntities)
+	// fmt.Println("Slice has one value: ", len(huffEntities) == 1)
 
 	generateHuffCodes(finalTree.root, "")
 
-	return getEncodedText(unencodedText), finalTree
+	return getEncodedText(unencodedText)
 }
 
 // Gets the smallest huffEntity in the slice and returns that and the modified slice
@@ -170,7 +227,8 @@ func getSmallestItem(arr []huffEntity) (huffEntity, []huffEntity) {
 		arr = append(arr[:index], arr[index+1:]...)
 	}
 
-	fmt.Println("This is supposedly the smallest item: ", smallestItem, ". This is the slice without it: ", arr)
+	// Change the following code to an assert perhaps
+	// fmt.Println("This is supposedly the smallest item: ", smallestItem, ". This is the slice without it: ", arr)
 	return smallestItem, arr
 }
 
@@ -188,13 +246,93 @@ func generateHuffCodes(entity huffEntity, huffcode string) {
 	}
 }
 
-func getEncodedText(unencodedText []byte) string {
+func getEncodedText(unencodedText []byte) []byte {
+	b := &bytes.Buffer{}
+	writer := bitio.NewWriter(b)
+
 	for _, item := range unencodedText {
-		fmt.Println(string(item))
-		// char := string(item)
+		char := string(item)
+		code := huffCodes[char]
 		// NOTE: Use bit writer and append bit by bit
+		for _, letter := range code {
+			if string(letter) == "1" {
+				_ = writer.WriteBool(true)
+			} else if string(letter) == "0" {
+				_ = writer.WriteBool(false)
+			} else {
+				panic("Invalid huffCode")
+			}
+		}
 	}
-	return "" // return the result
+
+	return b.Bytes()
+}
+
+func Decode(fileName string, jsonFileName string) []byte {
+	var res []byte
+
+	// read the json file
+	jsonData, err := os.ReadFile(jsonFileName)
+	if err != nil {
+		panic(err)
+	}
+	var codeStruct JsonCodes
+	if err := json.Unmarshal(jsonData, &codeStruct); err != nil {
+		fmt.Println("Failed to unmarshal json file.")
+		os.Exit(1)
+	}
+	// for verification
+	// fmt.Println("Contents of the json file: ", codeStruct.Codes)
+
+	// read encoded file
+	file, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error reading encoded file: ", fileName)
+		os.Exit(1)
+	}
+	reader := bitio.NewReader(bytes.NewBuffer(file))
+
+	bit, err := reader.ReadBool()
+	strBuffer := ""
+	unEncodedText := ""
+
+	for err == nil {
+		// read bit
+		if bit {
+			strBuffer = strBuffer + "1"
+		} else {
+			strBuffer = strBuffer + "0"
+		}
+
+		// check if bit is in map
+		key, absent := checkForValue(codeStruct.Codes, strBuffer)
+		if !absent {
+			unEncodedText = unEncodedText + key
+			strBuffer = ""
+		}
+
+		bit, err = reader.ReadBool()
+	}
+
+	// fmt.Println("This is the unencoded text: ", unEncodedText)
+	res = []byte(unEncodedText)
+
+	return res
+}
+
+func checkForValue(mapping map[string]string, searchValue string) (string, bool) {
+
+	for key, value := range mapping {
+		if searchValue == value {
+			return key, false
+		}
+	}
+
+	return "", true
+}
+
+type JsonCodes struct {
+	Codes map[string]string `json:"jsonCodes"`
 }
 
 type node struct {
